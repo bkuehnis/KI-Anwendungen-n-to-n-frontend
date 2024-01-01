b767a940c3debb2ede8fe56bc366f1b828068799
bkuehnis <62989491+bkuehnis@users.noreply.github.com>
2024-01-01 22:42:24 +0100

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+  import "./styles.css";
+</script>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">KI-Anwendung Demo</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon" />
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="/appartment">Appartment Regression</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+<div class="container mt-3">
+  <slot />
+</div>
