8a1098b9cea251c27d30f199e2f8884557abd235
bkuehnis <62989491+bkuehnis@users.noreply.github.com>
2024-01-01 23:15:00 +0100

    small updates
    updated comment on main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to the demo project</h1>
-<p>This demo shows use cases of simple "KI-Anwendungen"</p>
+<p>This demo shows use cases of simple "KI-Anwendungen".</p>
--- src/store.js
+++
@@ -1,6 +0,0 @@
-import { writable } from "svelte/store";
-
-// user 
-export const base_url = writable({});
-//base_url.set('http://127.0.0.1:5000')
-base_url.set('https://kuhs.pythonanywhere.com')
